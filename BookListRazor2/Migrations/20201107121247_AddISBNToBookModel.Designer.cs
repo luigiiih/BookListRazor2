dae82c28243069c46285faaa015fc221dd51cbab
luigiiih <67022306+luigiiih@users.noreply.github.com>
2020-11-07 13:33:52 +0100

    Udated Book object and DB with ISBN/Added the Create New Book Page

--- BookListRazor2/Migrations/20201107121247_AddISBNToBookModel.Designer.cs
+++ BookListRazor2/Migrations/20201107121247_AddISBNToBookModel.Designer.cs
@@ -3,14 +3,16 @@ using BookListRazor2.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookListRazor2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201107121247_AddISBNToBookModel")]
+    partial class AddISBNToBookModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
