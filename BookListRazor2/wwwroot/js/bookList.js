ff6687f8dabf941fe189b53b3ceab01ff8b3e647
luigiiih <67022306+luigiiih@users.noreply.github.com>
2020-11-09 13:59:42 +0100

    Added Upsert functionality

--- BookListRazor2/wwwroot/js/bookList.js
+++ BookListRazor2/wwwroot/js/bookList.js
@@ -12,14 +12,14 @@ function loadDataTable() {
             "datatype": "json"
         },
         "columns": [
-            { "data": "name", "width": "20%",},
-            { "data": "author", "width": "20%" },
-            { "data": "isbn", "width": "20%" },
+            { "data": "name", "title": "Name", "width": "20%",},
+            { "data": "author", "title": "Author", "width": "20%" },
+            { "data": "isbn", "title": "ISBN", "width": "20%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                        <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                        <a href="/BookList/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                             Edit
                         </a>
                         &nbsp;
